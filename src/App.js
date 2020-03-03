60da14d0e7f7f7182604c4ffb34cae9abb4cc713
Daniel Hsieh <daniel.x.hsieh@gmail.com>
2020-03-03 15:01:06 -0500

    now username is changed via props, divcolor by state (one-way)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import UserInput from './Components/UserInput';
 import UserOutput from './Components/UserOutput';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [userInputState, setUserInputState ] = useState(
+    {
+        users:[
+          {username: 'Ael', inputcolor:'red'},
+          {username: 'Bur', inputcolor:'green'},
+          {username: 'Cid', inputcolor:'blue'}
+        ]
+    }
+  );
+
+  const switchInputStateHandler = () => {
+    setUserInputState(
+      {
+        users:[
+          {username: 'Arachnae', inputcolor:'pink'},
+          {username: 'Boringer', inputcolor:'chartreuse'},
+          {username: 'Carthule', inputcolor:'aquamarine'}
+        ]
+      }
+    )
+  }
+
   return (
     <div className="App">
       <UserInput></UserInput>
-      <UserOutput username="Ael"></UserOutput>
-      <UserOutput username="Bur"></UserOutput>
-      <UserOutput username="Cid"></UserOutput>
+      <button onClick={switchInputStateHandler}>Switch Username and Input Color</button>
+      <div style={{backgroundColor: userInputState.users[0].inputcolor}}><UserOutput username={userInputState.users[0].username} inputcolor={userInputState.users[0].inputcolor}></UserOutput></div>
+      <div style={{backgroundColor: userInputState.users[1].inputcolor}}><UserOutput username={userInputState.users[1].username} inputcolor={userInputState.users[1].inputcolor}></UserOutput></div>
+      <div style={{backgroundColor: userInputState.users[2].inputcolor}}><UserOutput username={userInputState.users[2].username} inputcolor={userInputState.users[2].inputcolor}></UserOutput></div>
     </div>
   );
 }
